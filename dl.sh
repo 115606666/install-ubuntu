db23ebf584a8b1e2285a82c93aa3bbf0ae55bf24
mike <mike@box29>
2017-04-01 15:15:30 +0800

    Add executable permission to script files.

--- dl.sh
+++ dl.sh
@@ -4,3 +4,5 @@ wget https://raw.githubusercontent.com/115606666/install-ubuntu/master/install-0
 wget https://raw.githubusercontent.com/115606666/install-ubuntu/master/install-02.sh
 wget https://raw.githubusercontent.com/115606666/install-ubuntu/master/install-03.sh
 wget https://raw.githubusercontent.com/115606666/install-ubuntu/master/install-04.sh
+
+chmod +x install-*.sh
