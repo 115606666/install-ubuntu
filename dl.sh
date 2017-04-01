e62daca1c7534ff8e3e8a9ccafe9bfad0732d510
mike <mike@box29>
2017-04-01 15:09:53 +0800

    Add download script.

---
+++ dl.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+wget https://raw.githubusercontent.com/115606666/install-ubuntu/master/install-01.sh
+wget https://raw.githubusercontent.com/115606666/install-ubuntu/master/install-02.sh
+wget https://raw.githubusercontent.com/115606666/install-ubuntu/master/install-03.sh
+wget https://raw.githubusercontent.com/115606666/install-ubuntu/master/install-04.sh
