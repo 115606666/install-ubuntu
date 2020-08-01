e18fdf0ac7d66c5b9b4d1c9ab0a9bd0e6e89e8cc
mike <mike@box43>
2020-08-01 17:49:18 +0800

    commit

--- dl.sh
+++ dl.sh
@@ -4,5 +4,8 @@ wget https://raw.githubusercontent.com/115606666/install-ubuntu/master/install-0
 wget https://raw.githubusercontent.com/115606666/install-ubuntu/master/install-02.sh
 wget https://raw.githubusercontent.com/115606666/install-ubuntu/master/install-03.sh
 wget https://raw.githubusercontent.com/115606666/install-ubuntu/master/install-04.sh
+wget https://raw.githubusercontent.com/115606666/install-ubuntu/master/init-installer.sh
+wget https://raw.githubusercontent.com/115606666/install-ubuntu/master/install-ubuntu.sh
+wget https://raw.githubusercontent.com/115606666/install-ubuntu/master/setup-ubuntu.sh
 
 chmod +x install-*.sh
